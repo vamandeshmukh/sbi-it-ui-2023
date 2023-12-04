d4ff8372f61bd6f6a1ad4304d9558da8fc8d9974
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-12-04 15:25:48 +0530

    sbi it ui codes

--- 11-js-intro.js
+++ 11-js-intro.js
@@ -1,6 +1,63 @@
 // on cmd run - 
 // node 11-js-intro.js
 
+// console.log('Hello world!');
+// console.log('Hello world! 2')
+// console.log("Hello world! 3");
+// console.log(`Hello world! 4`);
 
-console.log('Hello world!');
+// console.log(1 + 2);
+
+// ===========
+// JS basics 
+// ===========
+
+// datatypes in JS 
+// number, string, boolean 
+
+// java 
+// int num; // strongly typed language 
+
+// JS 
+// let num = 'abc' ; // dynamically typed language 
+
+// variable declaration in JS 
+
+// num = 10; // do not use this 
+// var num2 = 20; // do not use this 
+// const num3 = 30; // use this as default choice 
+// let num4 = 40; // use this also 
+
+// variables and types - dynamic type inference 
+
+let num = 10;
+console.log(num);
+console.log(typeof (num));
+num = 20;
+console.log(num);
+console.log(typeof (num));
+num = 'abc';
+console.log(num);
+console.log(typeof (num));
+num = false;
+console.log(num);
+console.log(typeof (num));
+num = 30;
+console.log(num);
+console.log(typeof (num));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
