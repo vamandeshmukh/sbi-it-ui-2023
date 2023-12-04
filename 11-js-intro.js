7c2408a1dbc450853bb0f6bcddcedb70a5f2f75f
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-12-04 15:56:28 +0530

    sbi it ui codes

--- 11-js-intro.js
+++ 11-js-intro.js
@@ -30,21 +30,71 @@
 
 // variables and types - dynamic type inference 
 
-let num = 10;
-console.log(num);
-console.log(typeof (num));
-num = 20;
-console.log(num);
-console.log(typeof (num));
-num = 'abc';
-console.log(num);
-console.log(typeof (num));
-num = false;
-console.log(num);
-console.log(typeof (num));
-num = 30;
-console.log(num);
-console.log(typeof (num));
+// let num = 10;
+// console.log(num);
+// console.log(typeof (num));
+// num = 20;
+// console.log(num);
+// console.log(typeof (num));
+// num = 'abc';
+// console.log(num);
+// console.log(typeof (num));
+// num = false;
+// console.log(num);
+// console.log(typeof (num));
+// num = 30;
+// console.log(num);
+// console.log(typeof (num));
+
+// operators 
+// control structure 
+// 
+
+// truthy, falsy 
+// falsy - false, 0, '', undefined, null  
+// truthy - other than falsy 
+
+// let firstName = '';
+// if (firstName)
+//     console.log('yes');
+
+// relational operators 
+// console.log( 10 == 10);
+// console.log( 10 == '10');
+// console.log( 10 === '10');
+// console.log( 10 !== '10');
+
+// arrays and objects in JS 
+
+// const myArr = [10, 20, 'abc', false, ['a', 2, false], '30'];
+
+// for (let elem of myArr) {
+//     console.log(elem);
+// }
+
+
+const sbiEmp = {
+    eid: 101,
+    designation: 'AGM',
+    firstName: 'Sonu',
+    salary: 10.5,
+    isMarried: false,
+    address: {
+        city: 'Hyderabad',
+        pin: 500001
+    },
+    phones: [987600000, 543200000]
+    ,
+    bio: '@sonu'
+};
+
+// console.log(sbiEmp);
+console.log(sbiEmp.firstName);
+console.log(sbiEmp.phones[1]);
+
+
+
+
 
 
 
