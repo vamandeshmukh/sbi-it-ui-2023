fe15d9465e022e54e312e02034e796dff63b4f6c
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-12-05 10:48:30 +0530

    sbi ui day 2

--- 13-js-modern.js
+++ 13-js-modern.js
@@ -1,3 +1,23 @@
-old
+// old js
+// modern js next gen js 
+// JS  
+// ES - EcmaScript  
+
+class Person {
+    
+}
+
+class Employee extends Person {
+
+}
+
+// java 
+// Employee obj = new Employee();
+
+// JS 
+const obj = new Employee();
+
+
+
 
 
